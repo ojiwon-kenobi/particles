c24561d100906a8f22f61d5152984ca30dd16e3d
Jiwon Lee <jiwon.lee@berkeley.edu>
2020-12-25 19:28:57 -0600

    void

--- script.js
+++ script.js
@@ -34,24 +34,26 @@ class Particle {
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size,0,Math.PI*2, false);
-        ctx.fillStyle = this.color;
+        var gradient = ctx.createRadialGradient(this.x, this.y, this.size*4/5, this.x, this.y, this.size);
+
+        // Add color stops
+        gradient.addColorStop(0, "rgba(255,255,255,0.8)");
+        gradient.addColorStop(0.9, "#00b140");
+        ctx.fillStyle = gradient;
+
         ctx.fill();
     }
     update() {
         this.size-=(Math.random()*0.1); //shrink particle little by litte
         if(this.size < 0) { //if the particles are smaller than zero, tey'll appear where the mouse is at the mment
-            this.x= Math.random(0, canvas.width)*1000; //interactive
-            this.y= Math.random(canvas.height*4/5, canvas.height)*1000; 
-            this.size = (Math.random() * 10) + 2;
-            this.weight = (Math.random() * 2)-0.5;
+            this.x= Math.random()*canvas.width; //interactive
+            this.y= Math.random()*(canvas.height);/* - canvas.height*2/3) + canvas.height*2/3;*/ 
+            this.size = (Math.random() * 12) + 6;
+            this.weight = (Math.random())-0.5;
         }
-        this.y -= this.weight/2;//want it to rise
+        this.y -= this.weight/4;//want it to rise
         this.weight += 0.02; //want it ...to rise faster? //rising should not be accelerated tho
-
-        if((this.y > canvas.height - this.size) /*&& (this.weight > 0.5)*/) {
-            this.weight /= 0.05; //rise!
-        };
-    }
+        }
 }
 
 function init() {
@@ -59,8 +61,8 @@ function init() {
     for(let i =0; i < numberOfParticles; i++) {
         let x= Math.random() * canvas.width;
         let y = Math.random() * canvas.height;
-        let size = (Math.random()* 5) + 2;
-        let color = 'black';
+        let size = (Math.random()* 5) + 4;
+        let color = 'red';
         let weight = 1;
         particleArray.push(new Particle(x, y, size, color, weight));
     }
